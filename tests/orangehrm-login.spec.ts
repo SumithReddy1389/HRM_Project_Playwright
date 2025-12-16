08810630cca01e91aa189c1638ef003adae45f63
SumithReddy1389 <muskuSumithReddy1389@gmail.com>
2025-12-16 09:07:51 +0530

    third commit

--- tests/orangehrm-login.spec.ts
+++ tests/orangehrm-login.spec.ts
@@ -3,8 +3,9 @@ import { OrangeHRMLoginPage } from "../pages/OrangeHRMLoginPage";
 
 test("OrangeHRM Admin login with valid credentials", async ({ page }) => {
     const loginPage = new OrangeHRMLoginPage(page);
-    await loginPage.openUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    await loginPage.login("Admin", "admin123");
+    await loginPage.openUrl("/");
+
+    await loginPage.login(process.env.USERNAME || "Admin", process.env.PASSWORD || "admin123");
 
   // Assert successful login by checking for Admin dashboard element
   await expect(page.locator('h6:has-text("Dashboard")')).toBeVisible();
